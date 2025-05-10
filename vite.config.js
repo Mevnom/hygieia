900cb5e9db06b47058800a135b0c5fa012e5fe2c
Mukhtar <yagboyajumukhtar2008@gmail.com>
2025-05-10 14:28:36 +0100

    zzzz

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hygieia/',  // Replace 'hygieia' with your actual repo name
-})
-
+  base: '/hygieia/', // This must match the GitHub repo name
+});
