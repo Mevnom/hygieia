e5c4576f2500a7560533d0651645f71d6e49f291
Mukhtar <yagboyajumukhtar2008@gmail.com>
2025-05-12 14:03:56 +0100

    4th commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/hygieia/', // This must match the GitHub repo name
+  base: "/hygieia/", // This must match the GitHub repo name
 });
