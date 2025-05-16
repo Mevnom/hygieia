5c3bd9e1270be866809315d5d2925d115b5426a3
Mukhtar <yagboyajumukhtar2008@gmail.com>
2025-05-16 16:34:11 +0100

    for

--- src/UserHeader.jsx
+++ src/UserHeader.jsx
@@ -10,21 +10,21 @@ const welcomes = [
         country: "Nigeria",
         greeting: "Wagwan"
     },
-  /*   {
-        name: "chen",
-        country: "Japan",
-        greeting: "Konnichiwa"
-    },
-    {
-        name: "singh",
-        country: "India",
-        greeting: "Namaste"
-    },
-    {
-        name: "Mustapha",
-        country: "Saudi Arabia",
-        greeting: "Salam Alaykum"
-    }, */
+    /*   {
+          name: "chen",
+          country: "Japan",
+          greeting: "Konnichiwa"
+      },
+      {
+          name: "singh",
+          country: "India",
+          greeting: "Namaste"
+      },
+      {
+          name: "Mustapha",
+          country: "Saudi Arabia",
+          greeting: "Salam Alaykum"
+      }, */
 ];
 
 
@@ -33,15 +33,19 @@ function RegisteredUser() {
     return (
         <div className='header-deux'>
 
-            <div className='username-deux'>
+            <Link to={"/"} style={{
+                textDecoration: "none",
+            }}> <div className='username-deux'>
                 {welcomes.map((welcome) => (
-                    <Link to={"/"}><h3 key={welcome.id} className='username-deux'> {welcome.greeting} {/* &nbsp; */} <span className='username-deux'>{welcome.name}</span> <i class="fa-solid fa-bolt"></i></h3></Link>
+                    <h3 key={welcome.id} style={{
+                        textDecoration: "none",
+                    }}> {welcome.greeting} <span className='username-deux'>{welcome.name}</span> <i class="fa-solid fa-bolt"></i></h3>
                 ))}
-            </div>
+            </div></Link>
             <span>
-                <HamburgerMenu/>
+                <HamburgerMenu />
                 <i class="fa-solid fa-user"></i>
-               {/*  <i class="fa-solid fa-cart-shopping"></i>
+                {/*  <i class="fa-solid fa-cart-shopping"></i>
                 <i class="fa-solid fa-gear"></i> */}
             </span>
 
